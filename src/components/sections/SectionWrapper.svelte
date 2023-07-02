8f1e481e68cd7cdc123e598df43ab27762d37de9
DevCorvus <devcorvus.contact@gmail.com>
2023-07-02 15:21:23 -0400

    feat: backgrond gradient and move scale animation into sections

--- src/components/sections/SectionWrapper.svelte
+++ src/components/sections/SectionWrapper.svelte
@@ -2,6 +2,21 @@
     export let id: string;
 </script>
 
-<section {id} class="relative w-full h-screen flex flex-col gap-10 p-10">
-    <slot />
+<section {id} class="relative w-full h-screen p-10">
+    <div class="w-full h-full flex flex-col gap-10 fade-in">
+        <slot />
+    </div>
 </section>
+
+<style>
+    .fade-in {
+        animation: fadeIn 1s ease-in-out;
+    }
+
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+            transform: scale(0.9);
+        }
+    }
+</style>
