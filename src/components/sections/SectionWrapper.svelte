aaf42acce93f126d8a0020f53278453b5a3540b3
DevCorvus <devcorvus.contact@gmail.com>
2023-07-03 15:40:56 -0400

    feat: replace fade-in animation with transition on intersection

--- src/components/sections/SectionWrapper.svelte
+++ src/components/sections/SectionWrapper.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
     export let id: string;
     export let flexible = false;
+
+    let element: HTMLElement;
+    let intersecting = false;
+
+    onMount(() => {
+        if (element instanceof HTMLElement) {
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    if (entries[0].isIntersecting) {
+                        intersecting = true;
+                        observer.unobserve(element);
+                    }
+                },
+                { threshold: 0.4 }
+            );
+
+            observer.observe(element);
+            return () => observer.unobserve(element);
+        }
+    });
 </script>
 
 <section
+    bind:this={element}
     {id}
-    class={`relative w-full ${flexible ? 'h-auto' : 'h-screen'} p-10`}
+    class={`relative w-full ${flexible ? 'h-auto' : 'h-screen'} p-10 `}
 >
-    <div class="w-full h-full flex flex-col gap-10 fade-in">
+    <div
+        class={`w-full h-full flex flex-col gap-10 transition duration-1000 ${
+            intersecting ? '' : 'opacity-0 scale-90'
+        }`}
+    >
         <slot />
     </div>
 </section>
-
-<style>
-    .fade-in {
-        animation: fadeIn 1s ease-in-out;
-    }
-
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-            transform: scale(0.9);
-        }
-    }
-</style>
