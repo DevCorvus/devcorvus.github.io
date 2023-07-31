b3c45a882c85652d3f66d43e0b0714e2317f4d3d
DevCorvus <devcorvus.contact@gmail.com>
2023-07-31 13:00:05 -0400

    feat: github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import i18next from 'astro-i18next';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), svelte(), i18next()],
+    site: 'https://devcorvus.github.io',
 });
